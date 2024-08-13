ed4e326e731990a5a9f052281c7d2bf01262f6fd
developerNoman <noman.shafique@rakurai.io>
2024-08-13 18:31:28 +0500

    performance of task1 is done

--- task1.cpp
+++ task1.cpp
@@ -3,58 +3,53 @@
 #include <algorithm>
 using namespace std;
 
- int solution(vector<int> &A, int n){
-    int minimumResult=0;
-     for(int k=1;k<n;k++){
-        int sum1 = 0, sum2 = 0;
-    for(int i=0; i<k; i++){
-        sum1 += A[i];
+int solution(vector<int> &A) {
+    int n=A.size();
+    if (n <= 1) return 0; 
+    
+    int totalSum = 0;
+    for (int i = 0; i < n; i++) {
+        totalSum += A[i];
     }
-    for(int i=k; i<n; i++){
-        sum2 += A[i];
+    int minimumResult = abs(A[0]-(totalSum-A[0])); 
+    int sum1 = 0; 
+    
+    for (int k = 0; k < n - 1; k++) {
+        sum1 += A[k];
+        int sum2 = totalSum - sum1;
+        int result = abs(sum1 - sum2);
+        if (result < minimumResult) {
+            minimumResult = result;
+        }
     }
-    if(k==1){
-        minimumResult=abs(sum1-sum2);
-    }
-    int result=abs(sum1-sum2);
-       if(result<minimumResult){
-        minimumResult=result;
-       }
-    }
+    
     return minimumResult;
 }
 
 int main()
 {
     vector<int> A={3,1,2,4,3};
-    int n = A.size();
-    cout << "The result is : " << solution(A, n) << endl;
+    cout << "The result is : " << solution(A) << endl;
     return 0;
 }
 
-
-
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// using namespace std;
-
-// int difference(int A[], int n, int k){
-//     int sum1 = 0, sum2 = 0;
+//  int solution(vector<int> &A, int n){
+//     int minimumResult=0;
+//      for(int k=1;k<n;k++){
+//         int sum1 = 0, sum2 = 0;
 //     for(int i=0; i<k; i++){
 //         sum1 += A[i];
 //     }
 //     for(int i=k; i<n; i++){
 //         sum2 += A[i];
 //     }
-//     return abs(sum1 - sum2);
-// }
-
-// int main()
-// {
-//     int A[] = { 3,1,2,4,3 };
-//     int n = sizeof(A)/sizeof(A[0]);
-//     int k = 3;
-//     cout << "The result is : " << difference(A, n, k) << endl;
-//     return 0;
+//     if(k==1){
+//         minimumResult=abs(sum1-sum2);
+//     }
+//     int result=abs(sum1-sum2);
+//        if(result<minimumResult){
+//         minimumResult=result;
+//        }
+//     }
+//     return minimumResult;
 // }
