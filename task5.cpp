19568e176c7a3dec476363893863f114518ac037
developerNoman <noman.shafique@rakurai.io>
2024-08-17 15:11:37 +0500

    the task3 is 100% correct but the performance is not good

--- task5.cpp
+++ task5.cpp
@@ -4,13 +4,13 @@
 #include <algorithm>
 using namespace std;
 
+//function to check a triangle can be built from a given set of edges.
 int solution(vector<int> &A) {
     int n = A.size();
     if (n < 3) return 0;
     sort(A.begin(), A.end());
 
     for (int i = 0; i < n - 2; ++i) {
-      //  if (A[i] + A[i + 1] > A[i + 2]) {
         if (static_cast<long long>(A[i]) + A[i + 1] > A[i + 2]) {
             return 1;
         }
@@ -37,5 +37,28 @@ int main()
 //     }
 //     }
 //     }
+//     return 0;
+// }
+
+
+
+// int solution(vector<int> &A) {
+//     int n = A.size();
+//     if (n < 3) return 0;
+//     sort(A.begin(), A.end());
+
+//     for (int i = 0; i < n - 2; ++i) {
+//       //  if (A[i] + A[i + 1] > A[i + 2]) {
+//         if (static_cast<long long>(A[i]) + A[i + 1] > A[i + 2]) {
+//             return 1;
+//         }
+//     }
+
+//     return 0;
+// }
+// int main()
+// {
+//     vector<int> A={10,2,5,1,8,20};
+//     cout << "The result is : " << solution(A) << endl;
 //     return 0;
 // }
