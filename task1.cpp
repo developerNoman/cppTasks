b2d62a6174dec948ab47ff3a0ec68e375d761b38
developerNoman <noman.shafique@rakurai.io>
2024-08-17 14:33:33 +0500

    task1 is updated with comment and good naming

--- task1.cpp
+++ task1.cpp
@@ -2,32 +2,41 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int solution(vector<int> &A) {
-    int n=A.size();
-    if (n <= 1) return 0; 
-    
-    int totalSum = 0;
-    for (int i = 0; i < n; i++) {
-        totalSum += A[i];
+
+// function to return the minimum absolute difference between the sum of the first part and the sum of the second part.
+int solution(vector<int> &A)
+{
+    int length = A.size();
+    if (length <= 1){
+        return 0;
     }
-    int minimumResult = abs(A[0]-(totalSum-A[0])); 
-    int sum1 = 0; 
-    
-    for (int k = 0; k < n - 1; k++) {
-        sum1 += A[k];
+    int totalSum = 0;
+
+    for (int index = 0; index < length; index++)
+    {
+        totalSum += A[index];
+    }
+
+    int minimumResult = abs(A[0] - (totalSum - A[0]));
+    int sum1 = 0;
+
+    for (int index = 0; index < length - 1; index++)
+    {
+        sum1 += A[index];
         int sum2 = totalSum - sum1;
         int result = abs(sum1 - sum2);
-        if (result < minimumResult) {
+        if (result < minimumResult)
+        {
             minimumResult = result;
         }
     }
-    
+
     return minimumResult;
 }
 
 int main()
 {
-    vector<int> A={3,1,2,4,3};
+    vector<int> A = {3, 1, 2, 4, 3};
     cout << "The result is : " << solution(A) << endl;
     return 0;
 }
