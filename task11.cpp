ca4b234f5f168102300cac92e74cdc777b94dfbb
developerNoman <noman.shafique@rakurai.io>
2024-08-18 20:47:20 +0500

    the task11 is updated

--- task11.cpp
+++ task11.cpp
@@ -4,21 +4,20 @@
 #include <algorithm>
 using namespace std;
 
-
+//function to find maximum sum of a compact subsequence of array elements.
 int solution(vector<int> &A) {
-    int n = A.size();
-    if (n == 2) {
+    int length = A.size();
+    if (length == 2) {
         return max(A[0],max(A[1], A[0]+A[1]));
     }
 
-    int maxSum = A[0];//3
-    int currentSum = A[0];//3
+    int maxSum = A[0];
+    int currentSum = A[0];
 
-    for (int i = 1; i < n; i++) {
-        currentSum = max(A[i], currentSum + A[i]);//(2,5)->5,(-6,-1)->-1,(4,3)->4,(0,4)->4
-        maxSum = max(maxSum, currentSum);//(3,5)->5,(5,-1)->5,(5,4)->5,(5,4)------->5
+    for (int index = 1; index < length; i++) {
+        currentSum = max(A[i], currentSum + A[i]);
+        maxSum = max(maxSum, currentSum);
     }
-
     return maxSum;
 }
 int main()
@@ -44,5 +43,21 @@ int main()
 //         }
 //     }
 
+//     return maxSum;
+// }
+
+// int solution(vector<int> &A) {
+//     int n = A.size();
+//     if (n == 2) {
+//         return max(A[0],max(A[1], A[0]+A[1]));
+//     }
+
+//     int maxSum = A[0];//3
+//     int currentSum = A[0];//3
+
+//     for (int i = 1; i < n; i++) {
+//         currentSum = max(A[i], currentSum + A[i]);//(2,5)->5,(-6,-1)->-1,(4,3)->4,(0,4)->4
+//         maxSum = max(maxSum, currentSum);//(3,5)->5,(5,-1)->5,(5,4)->5,(5,4)------->5
+//     }
 //     return maxSum;
 // }
