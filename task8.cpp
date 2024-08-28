6ee5945dac1d690da9d8fbe07b00df4e105a4530
developerNoman <noman.shafique@rakurai.io>
2024-08-28 12:22:05 +0500

    the task 8 is updated

--- task8.cpp
+++ task8.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <stack>
 using namespace std;
 
-//function to find minimum number of blocks used to make the stone wall
-int solution(vector<int> &A) {
-    int length = A.size();
-    stack<int> blocks;
+
+int solution(const vector<int>& A) {
+    int length=A.size();
+    vector<int> blocks;
     int totalBlocks = 0;
-
-    for (int index = 0; index < length; index++) { 
-        int height = A[index];
-
-        while (!blocks.empty() && blocks.top() > height) {
-            blocks.pop();
+    
+    for (int height=0; height<length;height++) {
+        while (!blocks.empty() && blocks.back() > A[height]) {
+            blocks.pop_back();
         }
-        
-        if (blocks.empty() || blocks.top() < height) {
-            blocks.push(height);
+    
+        if (blocks.empty() || blocks.back() < A[height]) {
+            blocks.push_back(A[height]);
             totalBlocks++;
         }
     }
@@ -26,35 +22,12 @@ int solution(vector<int> &A) {
     return totalBlocks;
 }
 
-
-
-int main()
-{
-    vector<int> A={8,8,5,7,9,8,7,4,8};
-    cout << "The result is : " << solution(A) << endl;
+int main() {
+    vector<int> A = {8, 8, 5, 7, 9, 8, 7, 4, 8};
+    cout << "The result is: " << solution(A) << endl;
     return 0;
 }
 
-
-
-// }
-// int solution(vector<int> &A) {
-//     int n=A.size();
-//     if(n==0){
-//         return 0;
-//     }
-//     int count=0;
-//     for(int i=0;i<n;i++){
-//         for(int j=i+1;j>=0; j--)
-//         if(A[j]!=A[i]){
-//             count++;
-//             break;
-//         }
-//     }
-
-//     return count;
-    
-// }
 
 
 
@@ -78,19 +51,3 @@ int main()
 // }
 
 
-// int solution(vector<int> &A) {
-//     int n=A.size();
-//     stack<int> blocks;
-//     int totalBlocks = 0;
-
-//         for (int height=0;height<n;height++) {
-//             while (!blocks.empty() && blocks.top() > A[height-1]) {
-//                 blocks.pop();
-//             }
-//             if (blocks.empty() || blocks.top() < A[height-1]) {
-//                 blocks.push(A[height-1]);
-//                 totalBlocks++;
-//             }
-//     }=
-//     return totalBlocks;
-// }
